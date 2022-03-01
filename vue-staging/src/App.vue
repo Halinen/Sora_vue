ca9fa6fd4008336de1946313434490ec9326e167
rooy root <kurisushiina@gmail.com>
2022-03-01 18:48:12 +0800

    update

---
+++ vue-staging/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<div id='ss'>
+  <HelloWorld carName="carName"/>
+  <input type="text" ref="keyWord">
+  </div>
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default{
+  components:{HelloWorld},
+  data(){
+    return {
+      carName:'sss'
+    }
+  }
+}
+</script>
---
+++ vue-staging/src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h2>dajjdoisa:{{school}}</h2>
+  </div>
+
+</template>
+
+
+<script>
+  export default {
+    data(){
+      console.log('helloooo',this)
+      return {
+        school:'sss'
+      }
+    },
+  }
+</script >
